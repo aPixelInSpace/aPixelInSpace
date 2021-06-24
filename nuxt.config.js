6df2495cfc239010cf1e900ec6721e625a9f7fd5
Patrizio Amella <62834023+aPixelInSpace@users.noreply.github.com>
2021-06-24 16:53:01 +0200

    Changed blog name

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'blog',
+    title: "Patrizio Amella's blog",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
